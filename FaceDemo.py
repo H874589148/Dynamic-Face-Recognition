a157e4fbc9e25b9c0c9874d9e292254194a1b34d
Hawkeye <40380233+H874589148@users.noreply.github.com>
2019-02-02 22:50:03 +0800

    第一步完成

--- FaceDemo.py
+++ FaceDemo.py
@@ -2,13 +2,16 @@
 import cv2
 
 # 2.加载人脸模型
-cv2.imread('image1.jpg')
+img = cv2.imread('F:\github_repository\Dynamic-Face-Recognition\image1.jpg')
 
 # 3.创建窗口
 cv2.namedWindow('Hawkeye 窗口')
 
 # 4.显示图片
+cv2.imshow('Hawkeyetest',img)
 
 # 5.暂停窗口
+cv2.waitKey(0)
 
 # 6.关闭窗口
+cv2.destoryAllWindows()
